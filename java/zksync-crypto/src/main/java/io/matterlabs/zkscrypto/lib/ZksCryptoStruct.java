050619d64a685ef002ad0a190218ec1172d29fcb
Mykhailo Tiutin <miha.tiutin@vareger.com>
2020-09-15 12:58:06 +0300

    Fixed App package and indents

--- java/zksync-crypto/src/main/java/io/matterlabs/zkscrypto/lib/ZksCryptoStruct.java
+++ java/zksync-crypto/src/main/java/io/matterlabs/zkscrypto/lib/ZksCryptoStruct.java
@@ -4,31 +4,30 @@ import jnr.ffi.Runtime;
 import jnr.ffi.Struct;
 
 public abstract class ZksCryptoStruct extends Struct {
+    protected final Unsigned8[] data;
+    protected byte[] cachedData;
 
-	protected final Unsigned8[] data;
-	protected byte[] cachedData;
+    protected ZksCryptoStruct(Runtime runtime, int dataLength) {
+        super(runtime);
+        data = super.array(new Unsigned8[dataLength]);
+    }
 
-	protected ZksCryptoStruct(Runtime runtime, int dataLength) {
-		super(runtime);
-		data = super.array(new Unsigned8[dataLength]);
-	}
+    protected void flushCachedValues() {
+        this.cachedData = null;
+    }
 
-	protected void flushCachedValues() {
-		this.cachedData = null;
-	}
-
-	public byte[] getData() {
-		while (cachedData == null) {
-			synchronized (this) {
-				if (cachedData == null) {
-					cachedData = new byte[this.data.length];
-					for (int i = 0; i < this.data.length; i++) {
-						cachedData[i] = data[i].byteValue();
-					}
-				}
-			}
-		}
-		return cachedData;
-	}
+    public byte[] getData() {
+        while (cachedData == null) {
+            synchronized (this) {
+                if (cachedData == null) {
+                    cachedData = new byte[this.data.length];
+                    for (int i = 0; i < this.data.length; i++) {
+                        cachedData[i] = data[i].byteValue();
+                    }
+                }
+            }
+        }
+        return cachedData;
+    }
 
 }
